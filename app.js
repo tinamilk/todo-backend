7976ba20c637142bd819fb77fb00e9751994d733
Kristina <kristinamilkovic0@gmail.com>
2022-12-05 18:55:39 +0300

    Add recursive-readdir-sync, delete redirect

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
-import redirectRouter from './routes/todo.redirect.js';
-import getRouter from './routes/todo.get.js';
-import postRouter from './routes/todo.post.js';
-import patchRouter from './routes/todo.patch.js';
-import deleteRouter from './routes/todo.delete.js';
+import recursive from 'recursive-readdir-sync';
 import cors from 'cors';
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const port = config.get('Customer.dbConfig.port');
 
@@ -14,7 +12,7 @@ const app = express();
 const PORT = port || 3030;
 
 const corsOptions = {
-	origin: 'http://localhost:5432'
+	origin: 'http://localhost:5432',
 };
 
 app.use(cors(corsOptions));
@@ -24,10 +22,11 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', redirectRouter);
-app.use('/', getRouter);
-app.use('/', postRouter);
-app.use('/', patchRouter);
-app.use('/', deleteRouter);
+recursive(`${__dirname}/routes`)
+	.forEach(async (file) => {
+		const path = await import(file);
+		app.use('/', path.default);
+	});
+
 
 app.listen(PORT, () => console.log(`Its started ${new Date()} on ${PORT}`));
