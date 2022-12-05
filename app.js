caef97a65ff05c346989192cdac292b62d51e0a2
Kristina <kristinamilkovic0@gmail.com>
2022-12-05 19:01:15 +0300

    Add recursive-readdir-sync, delete redirect

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 import config from 'config';
 import express from 'express';
 import bodyParser from 'body-parser';
-import redirectRouter from './routes/todo.redirect.js';
-import getRouter from './routes/todo.get.js';
-import postRouter from './routes/todo.post.js';
-import patchRouter from './routes/todo.patch.js';
-import deleteRouter from './routes/todo.delete.js';
+import recursive from 'recursive-readdir-sync';
 const port = config.get('Customer.dbConfig.port');
 
 const app = express();
@@ -13,10 +9,10 @@ const PORT = port || 3030;
 
 app.use(bodyParser.json());
 
-app.use('/', redirectRouter);
-app.use('/', getRouter);
-app.use('/', postRouter);
-app.use('/', patchRouter);
-app.use('/', deleteRouter);
+recursive(`${__dirname}/routes`)
+	.forEach(async (file) => {
+		const path = await import(file);
+		app.use('/', path.default);
+	});
 
 app.listen(PORT, () => console.log(`Its started ${new Date()} on ${PORT}`));
