c78134ef3ec9fa54d2d4fdfb1b1e4467d17f61c9
Kristina <kristinamilkovic0@gmail.com>
2022-12-05 15:53:39 +0300

    Add order and pagination

--- routes/todo.get.js
+++ routes/todo.get.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import db from '../models/index.js';
 const Task = db.task;
+const Op = db.Sequelize.Op;
 
 const router = express.Router();
 
 router.get('/tasks/', (req, res) => {
 	const { filterBy, order, pp, page } = req.query;
-	const sorting = order === 'desc' ? 'desc' : 'asc';
+	const sorting = order === 'desc' ? 'DESC' : 'ASC';
 	const filter = !filterBy ? null : filterBy === 'done';
 
 	Task.findAll({
 		where: { isDone: typeof filter === 'boolean' ? filter : { [Op.ne]: null } },
-		order: ['createdAt', sorting],
+		order: [['createdAt', sorting]],
+		offset: (page - 1) * pp,
+		limit: pp
 	})
 		.then((data) => {
+			console.log(data.length);
 			res.status(200).send(data);
 		})
 		.catch((err) => {
